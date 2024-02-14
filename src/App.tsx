bf1f55b911708854e95698d3b08ccfe94c480029
Massimiliano Arena <milikkan5000@gmail.com>
2024-02-14 18:17:37 +0100

    esercizio

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state,setState]=useState<number|string>('Start')
+
+  function onClick(){
+    const randomNumber=Math.random()
+    setState(randomNumber)
+  }
+
+  return <>
+  <button onClick={onClick}>{state}</button>
+  </>
 }
 
 export default App;
